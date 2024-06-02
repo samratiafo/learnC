00f323db4e40b5174c4471877dd439527a04d8c6
Samrat Sur <samratiafo@gmail.com>
2024-06-02 15:06:21 +0530

    Third commit

---
+++ Basic/functionDemo.c
@@ -0,0 +1,24 @@
+/**
+ * Author:    Samrat Sur
+ * Created:   01.06.2024
+ * 
+ * (c) Copyright by Samrat Innovations.
+ **/
+
+#include<stdio.h>
+//Function Declaration or Prototype
+void display(void);
+
+void main(void){
+
+    //classfication of Function based on return type and argument
+
+    //calling an No Argument No Return Type function in function
+    display();
+
+}
+
+//Function Definition for No Argument No Return Type function
+void display(){
+    printf("No Argument No Return Type function\n");
+}
