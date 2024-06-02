bfc2b50a879e372a400da1c7354acf4848e570f4
Samrat Sur <samratiafo@gmail.com>
2024-06-02 15:56:34 +0530

    Fourth Commit

--- Basic/functionDemo.c
+++ Basic/functionDemo.c
@@ -16,9 +16,34 @@ void main(void){
     //calling an No Argument No Return Type function in function
     display();
 
+    //calling an With Argument No Return Type function in function
+    int returnVal=getInfo();
+
+    //calling an No Argument With Return Type function in function
+    setInfo(9748708278);
+
+    //calling an With Argument With Return Type function in function
+    int res=add(3,5);
+
 }
 
 //Function Definition for No Argument No Return Type function
 void display(){
     printf("No Argument No Return Type function\n");
+}
+//Function Definition for No Argument with Return Type function
+int getInfo(){
+    printf("No Argument With Return Type function\n");
+    return 0;
+}
+//Function Definition for With Argument No Return Type function
+void setInfo(long mobileNo){
+    long int mob=mobileNo;
+    printf("With Argument No Return Type function\n");
+}
+//Function Definition for With Argument With Return Type function
+int add(int a,int b){
+
+    printf("With Argument With Return Type function\n");
+    return (a+b);
 }
